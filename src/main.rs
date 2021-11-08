9b39091a8226132e638c39c66b51aeb545d79e6b
Conner Dunn <connerdunn7399@gmail.com>
2021-11-07 18:58:19 -0700

    feat: extend structopt for long term vision

--- src/main.rs
+++ src/main.rs
@@ -11,36 +11,59 @@ mod hash;
     name = "idup",
     about = "Find duplicate images using avg perceptual hash function"
 )]
-struct Opt {
-    /// Image file
-    #[structopt(parse(from_os_str))]
-    img: PathBuf,
-
-    /// Image file 2
-    #[structopt(parse(from_os_str))]
-    img2: Option<PathBuf>,
+enum Opt {
+    /// Given a path, calculate & store hashes of files in the db
+    Scan {
+        /// File or folder
+        #[structopt(parse(from_os_str))]
+        path: PathBuf,
+        #[structopt(short, long)]
+        recursive: bool,
+    },
+    /// Retrieve duplicates or near duplicates from the db
+    List,
+    /// Clean outdated data in the db
+    Clean,
+    /// Recompute hashes of files in db
+    Update,
+    /// Print information about a particular file
+    Info {
+        #[structopt(parse(from_os_str))]
+        file: PathBuf,
+    },
+    /// Print information about two files
+    Compare {
+        // TODO should I make this 2..n files?
+        // TODO should this & info be merged?
+        /// File 1
+        #[structopt(parse(from_os_str))]
+        img1: PathBuf,
+        /// File 2
+        #[structopt(parse(from_os_str))]
+        img2: PathBuf,
+    }
 }
 
 fn main() {
     let opt = Opt::from_args();
     println!("{:?}", opt);
 
-    match opt.img2 {
-        // one image as command line arg
-        // calcualte it's phash and print it
-        None => {
+    match opt {
+        // calculate it's phash and print it
+        Opt::Info{ file } => {
+            // TODO I need better error handling
             let img =
-                image::open(&opt.img).expect("Failed to open the file for the perceptual hash");
+                image::open(&file).expect("Failed to open the file for the perceptual hash");
             let ph = hash::phash::hash(img);
             println!("phash: {}", ph);
-            let data = read(&opt.img).expect("Failed to open the file for the sha256 hash");
+            let data = read(&file).expect("Failed to open the file for the sha256 hash");
             let sh = hash::sha256::hash(data);
-            println!("sha265: {}", sh);
+            println!("sha256: {}", sh);
         }
-        // two images as command line args
-        // calcualte both phashes, and dist
-        Some(img2) => {
-            let img = image::open(&opt.img)
+
+        // calculate both phashes, and dist
+        Opt::Compare{ img1, img2 } => {
+            let img = image::open(&img1)
                 .expect("Failed to open the first file for the perceptual hash");
             let hash1 = hash::phash::hash(img);
             println!("img1: {}", hash1);
@@ -52,6 +75,10 @@ fn main() {
 
             let diff = hash::hamming_dist(hash1, hash2);
             println!("diff: {}", diff);
+        }
+
+        _ => {
+            println!("This functionality is currently being worked on");
         }
     }
 }
