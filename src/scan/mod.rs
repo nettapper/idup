653610753a6645c40a2198681e3d17779eb845b6
Conner Dunn <connerdunn7399@gmail.com>
2022-01-23 14:35:17 -0700

    refactor: scan::process_path fn

--- src/scan/mod.rs
+++ src/scan/mod.rs
@@ -4,29 +4,30 @@ use std::fs::{metadata, read, read_dir};
 use crate::hash;
 
 pub fn process_path(path: PathBuf, recursive: bool) {
-    let meta = metadata(&path).expect("Failed to look up file metadata");
-    if meta.is_dir() {
-        let mut stack: Vec<PathBuf> = Vec::new();
-        stack.push(path);
+    let mut stack: Vec<PathBuf> = Vec::new();
+    stack.push(path);
 
-        while !stack.is_empty() {
-            let curr_dir = stack.pop().unwrap();
-            for entry in read_dir(&curr_dir).unwrap() {
-                let path = entry.unwrap().path();
-                if path.is_dir() {
-                    println!("dir={}", path.to_str().unwrap_or("cannot print path due to non-UTF8 chars"));
-                    if recursive {
-                        stack.push(path);
+    while !stack.is_empty() {
+        let curr = stack.pop().expect("Failed to process item becuase the stack is empty");
+        if curr.is_dir() {
+            if recursive {
+                for entry in read_dir(&curr).expect(format!("Failed to read contents of dir={:?}", &curr).as_str()) {
+                    match entry {
+                        Ok(path_buf) => stack.push(path_buf.path()),
+                        Err(err) => println!("Cannot process entry with err={}", err),
                     }
-                } else {
-                    println!("file={}", path.to_str().unwrap_or("cannot print path due to non-UTF8 chars"));
-                    let data = read(&path).expect("Failed to open the file for the sha256 hash");
-                    let sh = hash::sha256::hash(data);
-                    println!("\tsha256: {}", sh);
                 }
             }
+        } else {
+            println!("processing file={}", curr.to_str().unwrap_or("cannot print path due to non-UTF8 chars"));
+            let data = read(&curr).expect("Failed to open the file for the sha256 hash");
+            let sh = hash::sha256::hash(data);
+            println!("\tsha256: {}", sh);
         }
-    } else {
-        println!("file={}", path.to_str().unwrap_or("cannot print path due to non-UTF8 chars"));
     }
 }
+
+// fn is_img(path: PathBuf) {
+//     let meta = metadata(&path).expect("Failed to look up file metadata");
+//     let ft = meta.file_type();
+// }
